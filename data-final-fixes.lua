c8aba304bca4efcd3397b5d50401e37dd0df833d
Alexander <95149904+garrotte13@users.noreply.github.com>
2022-05-08 13:43:52 +0300

    Adding sound for RI air filters

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -290,3 +290,10 @@ if mods["bzzirconium"] then
 
 end
 
+local air_working_sound = {
+  sound = { filename = "__SpaceG__/sounds/air-purifier.ogg" },
+  idle_sound = { filename = "__base__/sound/idle1.ogg" },
+  apparent_volume = 1.2,
+}
+data.raw["assembling-machine"]["air-filter-rampant-industry"].working_sound = air_working_sound
+data.raw["assembling-machine"]["air-filter-2-rampant-industry"].working_sound = air_working_sound
